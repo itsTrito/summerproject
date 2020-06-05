aac4a7058b5f1cee1a9cbebfd697c08097ad31ee
DieuMachine <lindenb477@hotmail.com>
2020-06-05 18:43:33 -0400

    Base controller

---
+++ MyPlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public RigidBody body;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.getKeyDown("a"))
+        {
+            body.velocity.x = 1f;
+        }
+
+        if (Input.getKeyUp("a") || Input.getKeyUp("d"))
+        {
+            body.velocity.x = 0f
+        }
+
+        if (Input.getKeyDown("d"))
+        {
+            body.velocity.x = -1f;
+        }
+    }
+}
