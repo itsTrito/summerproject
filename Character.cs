86ad4a38e27412a4155a159d28f2109d4b9c4707
DieuMachine <lindenb477@hotmail.com>
2020-06-05 20:55:12 -0400

    Avancer systeme de déplacement

---
+++ Character.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character
+{
+    public bool IsWalk;
+    public bool IsArmed;
+    public bool IsJumping;
+
+    public float WalkSpeed;
+    public float ArmedWalkSpeed;
+
+    public float life;
+
+    public float jumpForce;
+
+    public Character(float walkSpeed, float armedWalkSpeed)
+    {
+        this.WalkSpeed = walkSpeed;
+        this.ArmedWalkSpeed = armedWalkSpeed;
+        this.IsWalk = false;
+        this.IsArmed = false;
+    }
+
+    public void setLife(float life)
+    {
+        this.life = life;
+    }
+
+    public void setJumpForce(float force)
+    {
+        this.jumpForce = force;
+    }
+}
--- MyPlayerController.cs
+++ MyPlayerController.cs
@@ -1,33 +1,72 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Movement : MonoBehaviour
+public class MyPlayerController : MonoBehaviour
 {
-    public RigidBody body;
+    public Rigidbody2D rb;
+    public SpriteRenderer sp;
+
+    private Vector2 _vel;
     
-    // Start is called before the first frame update
-    void Start()
+    public Character player;
+    
+    private void Start()
     {
-        
+        player = new Character(4f, 2f);
+        player.setLife(10f);
+        player.setJumpForce(2f);
     }
 
-    // Update is called once per frame
-    void FixedUpdate()
+    private void FixedUpdate()
     {
-        if (Input.getKeyDown("a"))
+        if (rb.velocity.y != 0f)
+            _vel.y += -1f * Time.deltaTime;
+        else
         {
-            body.velocity.x = 1f;
+            _vel.y = 0f;
+            player.IsJumping = false;
         }
 
-        if (Input.getKeyUp("a") || Input.getKeyUp("d"))
+        if (Input.GetKey(KeyCode.D) && !player.IsJumping)
         {
-            body.velocity.x = 0f
+            player.IsWalk = true;
+            _vel.x = (!player.IsArmed?player.WalkSpeed:player.ArmedWalkSpeed);
+            FlipX(false);
         }
 
-        if (Input.getKeyDown("d"))
+        if (Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.A))
         {
-            body.velocity.x = -1f;
+            _vel.x = 0f;
+            player.IsWalk = false;
         }
+
+        if ((Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D)) && !player.IsJumping)
+        {
+            player.IsWalk = true;
+        }
+
+        if (Input.GetKey(KeyCode.A) && !player.IsJumping)
+        {
+            player.IsWalk = true;
+            _vel.x = -(!player.IsArmed?player.WalkSpeed:player.ArmedWalkSpeed);
+            FlipX(true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && _vel.y == 0f)
+        {
+            _vel.y = player.jumpForce;
+            player.IsWalk = false;
+            player.IsJumping = true;
+        }
+
+        rb.velocity = _vel;
+        //if walk play animation
+    }
+
+    private void FlipX(bool flip)
+    {
+        sp.flipX = flip;
     }
 }
